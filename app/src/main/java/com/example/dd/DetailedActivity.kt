fa4c0c5c329c38a24725d5e740e9f4a5d0a08591
Beyza Usta <129887098+beyzqusta@users.noreply.github.com>
2024-06-23 15:34:29 +0300

    DoktorProfilleri

---
+++ app/src/main/java/com/example/dd/DetailedActivity.kt
@@ -0,0 +1,29 @@
+package com.example.dd
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetailedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detailed)
+        val getData = intent.getParcelableExtra<DataClass>("android")
+        if (getData != null) {
+            val detailTitle: TextView = findViewById(R.id.detailTitle)
+            val detailDesc: TextView = findViewById(R.id.detailDesc)
+            val detailImage: ImageView = findViewById(R.id.detailImage)
+
+            detailTitle.text = getData.dataTitle
+            detailDesc.text = getData.dataDesc
+            detailImage.setImageResource(getData.dataDetailImage)
+        }
+
+
+    }
+}
